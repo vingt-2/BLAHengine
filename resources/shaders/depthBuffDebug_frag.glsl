697de4ba0b3539666f4d81f5a945876440a937d9
Vincent P <vincent.petrella@mail.mcgill.ca>
2016-05-31 16:52:12 -0400

    renamed shaders

---
+++ resources/shaders/depthBuffDebug_frag.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+
+// Ouput data
+layout(location = 0) out vec4 color;
+
+uniform sampler2D texture;
+
+in vec2 UV;
+
+void main(){
+	float depthColor = texture2D(texture, UV).r;
+	depthColor = 100 * (1 - depthColor);
+	color = vec4(depthColor,depthColor,depthColor,1);
+}
